c041fd63d007011a102733fd67cb855878d29e6e
Agustinus Ignat Deswanto <ignat.deswanto@getplus.id>
2018-10-23 15:03:31 +0700

    Master source code EO Hubs

---
+++ app/src/main/java/com/artolanggeng/eohubs/MainActivity.java
@@ -0,0 +1,13 @@
+package com.artolanggeng.eohubs;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
